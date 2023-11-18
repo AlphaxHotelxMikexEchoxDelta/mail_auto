6ee585ae49e71bd158e69d3df829afb6aa2ff2b6
Ahmed <95902084+AlphaxHotelxMikexEchoxDelta@users.noreply.github.com>
2023-11-18 20:37:58 +0100

    Update mail.py

--- mail.py
+++ mail.py
@@ -2,49 +2,52 @@ import pyautogui as pia
 import pyperclip as pyp
 from time import sleep
 
-mails = open("mail.txt","r")
-lines = mails.readlines()
-for line in lines:
-    mail = line.replace("\n","")
-    
-    sleep(5)
-    # nouveau mail
-    pia.click(1778,361)
+try:
+    mails = open("popo.txt","r")
+    lines = mails.readlines()
+    for line in lines:
+        mail = line.replace("\n","")
+        
+        sleep(5)
+        # nouveau mail
+        pia.click(1778,361)
 
-    sleep(0.5)
-    # destinataire
-    pia.click(1953,587)
-    pyp.copy(mail)
-    pia.hotkey("ctrl", "v")
+        sleep(0.5)
+        # destinataire
+        pia.click(1953,587)
+        pyp.copy(mail)
+        pia.hotkey("ctrl", "v")
 
-    sleep(0.5)
-    # objet
-    pia.click(1822,789)
-    pyp.copy("[Candidature spontanée][Génie informatique]")
-    pia.hotkey("ctrl", "v")
+        sleep(0.5)
+        # objet
+        pia.click(1822,789)
+        pyp.copy("[Candidature spontanée][Génie informatique]")
+        pia.hotkey("ctrl", "v")
 
-    sleep(0.5)
-    # message
-    pia.click(1908,888)
-    pyp.copy("""
-MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS 
-MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS 
-MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS 
-MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS 
-MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS 
-MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS 
-    """)
-    pia.hotkey("ctrl", "v")
+        sleep(0.5)
+        # message
+        pia.click(1908,888)
+        pyp.copy("""
+    MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS 
+    MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS 
+    MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS 
+    MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS 
+    MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS 
+    MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS 
+        """)
+        pia.hotkey("ctrl", "v")
 
-    sleep(0.5)
-    # CV calendrier
-    pia.click(2656,362)
-    pia.click(2486,825)
-    pia.click(1967,1277)
-    pia.click(2656,362)
-    pia.click(2486,825)
-    pia.click(1768,1372)
+        sleep(0.5)
+        # CV calendrier
+        pia.click(2656,362)
+        pia.click(2486,825)
+        pia.click(1967,1277)
+        pia.click(2656,362)
+        pia.click(2486,825)
+        pia.click(1768,1372)
 
-    sleep(5)
-    # envoi
-    pia.click(1702,471)
+        sleep(5)
+        # envoi
+        pia.click(1702,471)
+except IOError as e:
+    print("Oopsiii, fichier introuvable ! (~_~)")

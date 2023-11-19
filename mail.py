1bfd3068cdf35912c30c06921abeb687f05f2755
Ahmed <95902084+AlphaxHotelxMikexEchoxDelta@users.noreply.github.com>
2023-11-19 18:11:55 +0100

    Update mail.py

--- mail.py
+++ mail.py
@@ -3,51 +3,59 @@ import pyperclip as pyp
 from time import sleep
 
 try:
-    mails = open("popo.txt","r")
+    mails = open("mail.txt","r")
     lines = mails.readlines()
+
+    cc = len(lines)
+
     for line in lines:
+
+        cc = cc - 1
+        print("--- {}mails restant ---".format(cc))
+
         mail = line.replace("\n","")
-        
-        sleep(5)
+        print("{} [en cours...]".format(mail))
+
+        sleep(1)
         # nouveau mail
         pia.click(1778,361)
 
-        sleep(0.5)
+        sleep(1)
         # destinataire
         pia.click(1953,587)
         pyp.copy(mail)
         pia.hotkey("ctrl", "v")
 
-        sleep(0.5)
+        sleep(1)
         # objet
         pia.click(1822,789)
         pyp.copy("[Candidature spontanée][Génie informatique]")
         pia.hotkey("ctrl", "v")
 
-        sleep(0.5)
+        sleep(1)
         # message
         pia.click(1908,888)
         pyp.copy("""
-    MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS 
-    MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS 
-    MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS 
-    MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS 
-    MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS 
-    MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS MAILS 
+MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL
+MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL
+MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL
+MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL
+MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL
+MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL
+MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL MAIL
         """)
         pia.hotkey("ctrl", "v")
 
-        sleep(0.5)
+        sleep(1)
         # CV calendrier
         pia.click(2656,362)
         pia.click(2486,825)
-        pia.click(1967,1277)
-        pia.click(2656,362)
-        pia.click(2486,825)
+        sleep(0.5)
         pia.click(1768,1372)
 
-        sleep(5)
+        sleep(4)
         # envoi
         pia.click(1702,471)
+        print("{} [Envoye]\n".format(mail))
 except IOError as e:
     print("Oopsiii, fichier introuvable ! (~_~)")
